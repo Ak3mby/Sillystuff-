f4146e6c69d173d62a110f45b5772c8c3f3c9add
Akemby <jean.thirtyone@gmail.com>
2024-10-18 14:24:50 -0500

    Update Black font for Animeflv.user.js

--- Black font for Animeflv.user.js
+++ Black font for Animeflv.user.js
@@ -4,7 +4,7 @@
 // @description    No need for userstyles, just crappy gui with plain black background
 // @version        test1
 // @author         Ak3mby(Akemby)
-// @icon           https://media.discordapp.net/attachments/1201846119592898650/1295509104311275581/fun-lil-icon-i-made-v0-6901euj8t7ab1.png?ex=670ee85f&is=670d96df&hm=94989f9e9bc49c58062a8366aeed8f4588840b764d42990a5645f87828f704e4&=&format=webp&quality=lossless&width=428&height=428
+// @icon           https://cdn.discordapp.com/icons/1296915461954470058/bf720ede6b8c239672ce5d2e4873ae14.webp?size=100&quo
 // @match          https://www3.animeflv.net/*
 // ==/UserScript==
 let font = document.querySelector("head > link:nth-child(15)")
